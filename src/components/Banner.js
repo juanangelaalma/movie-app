3ff82822c9b6c6ff564f19d581dc4faea63c3b2b
juanangelaalma <juanlah77@gmail.com>
2022-01-30 23:08:00 +0700

    add continues watching

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Banner = () => {
   return (
-    <div className='w-full h-40 md:h-60 bg-orange-600 overflow-hidden rounded-xl relative'>
+    <div className='w-full h-40 md:h-60 w-full bg-orange-600 overflow-hidden rounded-xl relative'>
       <img src="https://cdn2.geeq.id/media/post/medium/5863-ada-hulkbuster-di-poster-baru-avengers-end-game-begini-penampakannya.jpg" className='w-full h-full object-cover object-center' alt="Banner" />
       <button className="px-4 py-2 absolute bottom-4 right-4 rounded-lg bg-orange hover:bg-orange-hover text-white font-bold">Watch now</button>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner";
+import ContinueWatching from "./ContinueWatching";
 
 const Header = () => {
   return (
-    <div className="flex">
+    <div className="flex pt-2 flex-wrap">
       <Banner />
+      <ContinueWatching />
     </div>
   );
 };
