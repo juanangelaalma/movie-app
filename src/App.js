8c1bc222bdfdfb7de34d1773c8b90dab0cf89801
juanangelaalma <juanlah77@gmail.com>
2022-01-31 00:03:36 +0700

    add genres

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Header, PopularMovies } from "./components";
+import { Navbar, Header, PopularMovies, Genres } from "./components";
 
 const App = () => {
   return (
@@ -11,6 +11,13 @@ const App = () => {
         </div>
         <div className="w-100 md:basis-2/3 m-0 pb-4 px-6 md:px-0">
           <Header />
+        </div>
+      </div>
+      <div className="w-100 container mx-auto flex flex-col-reverse md:flex-row pb-0 px-0">
+        <div className="w-100 md:basis-1/3 m-0 pb-4 px-6 md:px-0">
+          <Genres />
+        </div>
+        <div className="w-100 md:basis-2/3 m-0 pb-4 px-6 md:px-0">
         </div>
       </div>
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import Header from "./Header";
 import PopularMovies from "./PopularMovies";
+import Genres from "./Genres";
 
-export { Navbar, Header, PopularMovies }
+export { Navbar, Header, PopularMovies, Genres }
