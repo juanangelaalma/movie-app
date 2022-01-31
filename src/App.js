ab2b8a5e261bc1772e7297bd0e7796fdac30e7c8
juanangelaalma <juanlah77@gmail.com>
2022-01-31 08:28:38 +0700

    change font size in genres

--- src/App.js
+++ src/App.js
@@ -13,11 +13,12 @@ const App = () => {
           <Header />
         </div>
       </div>
-      <div className="w-100 container mx-auto flex flex-col-reverse md:flex-row pb-0 px-0">
+      <div className="w-100 container mx-auto flex flex-col md:flex-row pb-0 px-0">
         <div className="w-100 md:basis-1/3 m-0 pb-4 px-6 md:px-0">
           <Genres />
         </div>
         <div className="w-100 md:basis-2/3 m-0 pb-4 px-6 md:px-0">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, soluta?
         </div>
       </div>
     </div>
--- src/components/Genres.js
+++ src/components/Genres.js
@@ -7,27 +7,27 @@ const Genres = () => {
       <div className="whitespace-nowrap overflow-visible md:whitespace-pre-wrap md:overflow-visible space-x-3 md:space-x-0 md:flex md:flex-wrap w-full pt-4 overflow-x-scroll">
         <div className="md:pr-2 inline-block md:pb-2">
           <span className="bg-orange hover:bg-orange-hover px-2 py-1 rounded-xl inline-block">
-            <p className="text-white md:text-md font-light">Romance</p>
+            <p className="text-white md:text-md font-md">Romance</p>
           </span>
         </div>
         <div className="md:pr-2 inline-block md:pb-2">
           <span className="bg-blue-500 hover:bg-blue-700 px-2 py-1 rounded-xl inline-block">
-            <p className="text-white md:text-md font-light">Action</p>
+            <p className="text-white md:text-md font-md">Action</p>
           </span>
         </div>
         <div className="md:pr-2 inline-block md:pb-2">
           <span className="bg-green-500 hover:bg-green-700 px-2 py-1 rounded-xl inline-block">
-            <p className="text-white md:text-md font-light">Comedy</p>
+            <p className="text-white md:text-md font-md">Comedy</p>
           </span>
         </div>
         <div className="md:pr-2 inline-block md:pb-2">
           <span className="bg-red-500 hover:bg-red-700 px-2 py-1 rounded-xl inline-block">
-            <p className="text-white md:text-md font-light">Drama</p>
+            <p className="text-white md:text-md font-md">Drama</p>
           </span>
         </div>
         <div className="md:pr-2 inline-block md:pb-2">
           <span className="bg-gray-600 hover:bg-gray-700 px-2 py-1 rounded-xl inline-block">
-            <p className="text-white md:text-md font-light">Horror</p>
+            <p className="text-white md:text-md font-md">Horror</p>
           </span>
         </div>
       </div>
