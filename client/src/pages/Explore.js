b4e412cad5475a1fc86b5e7d5586f9b652c26fc4
juanangelaalma <juanlah77@gmail.com>
2022-02-09 16:41:43 +0700

    add add api request and render for popular movies in explore page

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPopularMovies } from "../actions/popularMovies";
 import { FormSearch } from "../components";
 import CardMovie from "../components/CardMovie";
+import getPopularMovies from "../services/getPopularMovies";
+import { isEmptyObject } from "../utils/isEmptyObject";
 
 const Explore = () => {
+  const popularMovies = useSelector(state => state.popularMovies)
+  const dispatch = useDispatch()
+
+  const setPopular = (movies) => {
+    dispatch(setPopularMovies(movies))
+  }
+
+  useEffect(() => {
+    if(isEmptyObject(popularMovies)) {
+      getPopularMovies(setPopular)
+    }
+  }, [])
+
+  console.log(popularMovies)
+
   return (
     <div className="bg-black flex flex-wrap justify-center w-full pt-24 pb-4 h-100">
       <FormSearch />
       <div className="container">
         <h1 className="text-lg text-white block px-5">Popular Movies</h1>
         <div className="flex flex-wrap md:flex-nowrap">
-          <div className="w-1/2 px-5 py-2 md:py-5">  
-            <CardMovie poster="https://upload.wikimedia.org/wikipedia/id/5/5c/Who_am_I_movie_poster.jpg" rating="9.0" title="Alading 2019" dateRelease="Dec 2019" />
-          </div>
-          <div className="w-1/2 px-5 py-2 md:py-5">  
-            <CardMovie poster="https://upload.wikimedia.org/wikipedia/id/1/12/Start-Up_2020.jpg" rating="9.0" title="Alading 2019" dateRelease="Dec 2019" />
-          </div>
-          <div className="w-1/2 px-5 py-2 md:py-5">  
-            <CardMovie poster="https://upload.wikimedia.org/wikipedia/id/1/13/Jumanji_Welcome_to_the_Jungle_Poster.jpg" rating="9.0" title="Alading 2019" dateRelease="Dec 2019" />
-          </div>
-          <div className="w-1/2 px-5 py-2 md:py-5">  
-            <CardMovie poster="https://cdn1-production-images-kly.akamaized.net/ByfoNPSMTMfPEtHmnQMFhgGP80Y=/640x853/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3635478/original/025116000_1637133546-253154135_2120128131476179_3401639978712735642_n.jpg" rating="9.0" title="Alading 2019" dateRelease="Dec 2019" />
-          </div>
+          { !isEmptyObject(popularMovies) && popularMovies.map((movie) => (
+            <div key={movie.id} className="w-1/2 px-5 py-2 md:py-5">  
+              <CardMovie poster={movie.image_url} rating={movie.rating} title={movie.title} dateRelease={movie.release} />
+            </div>
+          )) }
         </div>
       </div>
       <div className="hidden container">
