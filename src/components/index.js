88b570bdbb80e793613f87464593cb959f6fc5ee
juanangelaalma <juanlah77@gmail.com>
2022-02-09 16:13:54 +0700

    migrate client file and folder to subfolder 'client/'

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Navbar from "./Navbar";
-import Header from "./Header";
-import PopularMovies from "./PopularMovies";
-import Genres from "./Genres";
-import BestArtists from "./BestArtists";
-import FormSearch from "./FormSearch"
-
-export { Navbar, Header, PopularMovies, Genres, BestArtists, FormSearch }
--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-import Home from "./Home";
-import Explore from "./Explore";
-
-export { Home, Explore }
