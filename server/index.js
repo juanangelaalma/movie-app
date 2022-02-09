079c991c79f0cd30eadcb957a8caf5b8bd6dd4da
juanangelaalma <juanlah77@gmail.com>
2022-02-09 21:46:22 +0700

    add morgan for logging

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ require("dotenv").config()
 const express = require('express')
 const cors = require("cors")
 const app = express()
+const morgan = require("morgan")
 
 const movieRouter = require("./routes/movies")
 const genreRouter = require("./routes/genres")
 const port = process.env.PORT || 5000
 
 app.use(cors())
+app.use(morgan("dev"))
 
 app.use('/movies', movieRouter)
 app.use('/genres', genreRouter)
