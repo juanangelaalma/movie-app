a653ce11de0023a875bf50a2bdb2ac76b91d08c7
juanangelaalma <juanlah77@gmail.com>
2022-02-11 12:42:19 +0700

    change loader

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <span class="animate-ping absolute inline-flex rounded-full w-8 h-8 bg-orange opacity-75"></span>
+    <span className="animate-ping absolute inline-flex rounded-full w-8 h-8 bg-orange opacity-75"></span>
   );
 };
 
