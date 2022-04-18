849cb613fbc82cbdeeb1fe3713e1950ef6bb9e5c
juanangelaalma <juanlah77@gmail.com>
2022-04-18 13:34:35 +0700

    i dont know why:

--- server/config/headers.js
+++ server/config/headers.js
@@ -1,6 +1,8 @@
+require("dotenv").config()
+
 const defaultHeader = {
     'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
-    'x-rapidapi-key': '7ab93f9e4cmsh5d77bf205dc6e39p1a8b4cjsn378b140107b3'
+    'x-rapidapi-key': process.env.RAPIDAPI_KEY
 }
 
 module.exports = { defaultHeader }
